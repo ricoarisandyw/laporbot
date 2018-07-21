3c3a91b5b12cb252b4def94d17806afc1f4b3101
ricoarisandyw <arisandyrico@gmail.com>
2018-07-21 23:19:43 +0700

    versi Beta

--- src/LINEBot/KitchenSink/EventHandler/MessageHandler/TextMessageHandler.php
+++ src/LINEBot/KitchenSink/EventHandler/MessageHandler/TextMessageHandler.php
@@ -322,7 +322,8 @@ class TextMessageHandler implements EventHandler
         $query = "INSERT INTO public.report(
             user_id, created_date, report_date, status)
             VALUES ('".$profile['userId']."',  now(), now(), 'ACTIVE');";
-        $result = pg_query($query) or die('Query failed: ' . pg_last_error());
+        $result = pg_query($query);
+        //or die('Query failed: ' . pg_last_error());
 
         // Free resultset
         pg_free_result($result);
