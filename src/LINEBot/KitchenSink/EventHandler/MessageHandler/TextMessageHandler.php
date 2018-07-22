b1713f51b61b104ae4bd7b6947605e18e916f299
ricoarisandyw <arisandyrico@gmail.com>
2018-07-22 15:36:55 +0700

    Logic Fix

--- src/LINEBot/KitchenSink/EventHandler/MessageHandler/TextMessageHandler.php
+++ src/LINEBot/KitchenSink/EventHandler/MessageHandler/TextMessageHandler.php
@@ -37,6 +37,7 @@ use LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselTemplateBuilder;
 use LINE\LINEBot\MessageBuilder\TemplateBuilder\ConfirmTemplateBuilder;
 use LINE\LINEBot\MessageBuilder\StickerMessageBuilder;
 use LINE\LINEBot\MessageBuilder\ImageMessageBuilder;
+use dg\Twitter;
 
 class TextMessageHandler implements EventHandler
 {
@@ -81,7 +82,6 @@ class TextMessageHandler implements EventHandler
         $replyToken = $this->textMessage->getReplyToken();
         $this->logger->info("Got text message from $replyToken: $text");
 
-        //TODO: Check in database if client has ACTIVE status or not.
         switch (strtolower($text)) {
             case 'profile':
                 $userId = $this->textMessage->getUserId();
@@ -241,20 +241,20 @@ class TextMessageHandler implements EventHandler
                     "Terima kasih atas laporan anda. (moon wink)");
                 $this->deactiveReport($profile);
                 //TODO: Send to tweeter
-                error_log("Tweeting . . .");
-                $consumerKey = 'GmVRzrbtiDPFuhTGtRyCS0zM0';
-                $accessToken = '231752825-18xWsGdvvZcrywMBxVtk4i1d1AsIe1YzN6w7y7Bk';
-                $accessTokenSecret = 'SVWR1YihOOBkmJegPxmjeiGCXKolGEDJm3dv5W6m0prq0';
-                $consumerSecret = 'oWjkuqkvFI5ierKawjwG5aGtjbACnKYgr0ZS3y6LH9OX5muXQu';
-                $twitter = new Twitter($consumerKey, $consumerSecret, $accessToken, $accessTokenSecret);
-                error_log("Login Tweeter Success . . .");
-                try {
-                    $tweet = $twitter->send('I am fine'); // you can add $imagePath or array of image paths as second argument
-                    error_log("Tweet success . . .");
-                } catch (TwitterException $e) {
-                    echo 'Error: ' . $e->getMessage();
-                    error_log("Tweet Error . . ." . $e->getMessage());
-                }
+                // error_log("Tweeting . . .");
+                // $consumerKey = 'GmVRzrbtiDPFuhTGtRyCS0zM0';
+                // $accessToken = '231752825-18xWsGdvvZcrywMBxVtk4i1d1AsIe1YzN6w7y7Bk';
+                // $accessTokenSecret = 'SVWR1YihOOBkmJegPxmjeiGCXKolGEDJm3dv5W6m0prq0';
+                // $consumerSecret = 'oWjkuqkvFI5ierKawjwG5aGtjbACnKYgr0ZS3y6LH9OX5muXQu';
+                // $twitter = new Twitter($consumerKey, $consumerSecret, $accessToken, $accessTokenSecret);
+                // error_log("Login Tweeter Success . . .");
+                // try {
+                //     $tweet = $twitter->send('I am fine'); // you can add $imagePath or array of image paths as second argument
+                //     error_log("Tweet success . . .");
+                // } catch (TwitterException $e) {
+                //     echo 'Error: ' . $e->getMessage();
+                //     error_log("Tweet Error . . ." . $e->getMessage());
+                // }
             }else{
                 error_log("All Data Filled . . .");
                 $this->bot->replyText($replyToken, 
